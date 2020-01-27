a2b6b3a42f3a3d429f4f57bc75af7f722ee78e6b
Giulio Rossi <ciuffi9@mac.com>
2020-01-27 00:35:58 -0800

    fixup become sponsor on mobile

--- components/becomeSponsorButton.vue
+++ components/becomeSponsorButton.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
-    <img
-      src="~/assets/graphics/leafLeft.svg"
+  <div id="buttonWrapper">
+    <img src="~/assets/graphics/leafLeft.svg">
+    <a
+      id="sponzaButton"
+      href="mailto:logistics@nwplus.io?subject=cmd-f Sponsorship"
     >
-    <a id="sponzaButton" href="mailto:logistics@nwplus.io?subject=cmd-f Sponsorship">
       Become a Sponsor
     </a>
-    <img
-      src="~/assets/graphics/leafRight.svg"
-    >
+    <img src="~/assets/graphics/leafRight.svg">
   </div>
 </template>
 
@@ -19,7 +18,7 @@
   transition-duration: 0.2s;
   position: relative;
   bottom: 30px;
-  background: #2F4246;
+  background: #2f4246;
   border-radius: 120px;
   padding: 14px 50px;
   color: white;
@@ -28,15 +27,23 @@
   font-weight: 600;
   font-size: 24px;
   line-height: 31px;
-  color: #E9E8E0;
+  color: #e9e8e0;
 }
 #sponzaButton:hover {
-  color: #ECAA1E;
+  color: #ecaa1e;
 }
 @include until($tablet) {
   #sponzaButton {
-  padding: 8px 40px;
-  font-size: 1em;
+    padding: 8px 40px;
+    margin: 0 5px;
+    font-size: 1em;
+  }
+}
+@include until(323px) {
+  #sponzaButton {
+    padding: 8px 25px;
+    margin: 0;
+    font-size: 1em;
   }
 }
 </style>
