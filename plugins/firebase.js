16dfd8a8e2441b01b61ddf7ec6e5e02d82cbee54
andykwan1124 <woody.andygrant@gmail.com>
2020-06-24 15:26:39 -0700

    use projectID directly in firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,7 +7,7 @@ if (!firebase.apps.length) {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: "nwhacks-2019-dev",
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     measurementId: 'G-CL002TYWPV',
