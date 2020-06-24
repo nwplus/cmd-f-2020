bf7edc58cc1e889cd183b76bc2b93fa85d7fc370
Ryan Mehri <ryan.mehri1@gmail.com>
2020-06-24 13:42:08 -0600

    Add lighthouse github action and config

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'yarn start'
+    },
+    assert: {
+      preset: 'lighthouse:recommended'
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+}
